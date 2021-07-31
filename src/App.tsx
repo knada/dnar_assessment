ac6b2d91d412adc3ee820e85dd1dc91a1b6d6234
knada <nanaayimda@gmail.com>
2021-07-31 13:54:53 +0000

    Added react-router BrowserRouter container to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import styled, { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 import { GlobalStyles } from "./theme/global";
 import theme from "./theme";
 import Dashboard from "./components/Dashboard/Dashboard";
@@ -8,14 +9,16 @@ const AppContainer = styled.div``;
 
 const App: FC = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <>
-                <GlobalStyles />
-                <AppContainer>
-                    <Dashboard />
-                </AppContainer>
-            </>
-        </ThemeProvider>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <>
+                    <GlobalStyles />
+                    <AppContainer>
+                        <Dashboard />
+                    </AppContainer>
+                </>
+            </ThemeProvider>
+        </BrowserRouter>
     );
 };
 
