7d2ad37f7469403bfb78a0e4f7bdc62458a0f2ec
knada <nanaayimda@gmail.com>
2021-08-03 13:12:36 +0000

    Removed eventsApi reducer

--- src/application/store.ts
+++ src/application/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { coinApi } from "./services/coins";
-import { eventsApi } from "./services/events";
 
 const store = configureStore({
     reducer: {
         [coinApi.reducerPath]: coinApi.reducer,
-        [eventsApi.reducerPath]: eventsApi.reducer,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(coinApi.middleware),
