f3db63d3cf6018a1751780a1d5932e0070a0cb94
Daniel Addae <nanaayimda@gmail.com>
2021-08-05 17:26:48 +0000

    added Coinprice and styles

--- src/components/CoinPage/CoinDataVisualization/ChangePercentage/ChangePercentage.tsx
+++ src/components/CoinPage/CoinDataVisualization/ChangePercentage/ChangePercentage.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import styled from "styled-components";
 import TrendDown from "../../../SvgComponents/TrendDown";
 import TrendUp from "../../../SvgComponents/TrendUp";
-import { CoinPrice } from "../CoinDataVisualization.styles";
 
 type ChangeProps = {
     change?: "up" | "down";
@@ -16,13 +15,27 @@ type ChangePercentageProps = {
 const ChangeContainer = styled.div`
     display: flex;
     align-items: center;
+    margin: 1rem;
+`;
+
+const CoinPrice = styled.h3`
+    font-family: "Inter", "Roboto", sans-serif;
+    font-weight: 500;
+    font-size: 1.5rem;
+    color: white;
+    @media screen and (min-width: ${props => props.theme.breakpoints.lg}px) {
+        font-size: 2.5rem;
+    }
 `;
 
 const ChangeValue = styled.h4<ChangeProps>`
     color: ${props => (props.change === "up" ? "green" : "red")};
-    margin-left: 4rem;
-    margin-right: 1rem;
-    font-size: 1.5rem;
+    margin-left: 2rem;
+    @media screen and (min-width: ${props => props.theme.breakpoints.lg}px) {
+        margin-left: 4rem;
+        margin-right: 1rem;
+        font-size: 1.5rem;
+    }
 `;
 
 const ChangePercentage: FC<ChangePercentageProps> = ({
@@ -32,7 +45,7 @@ const ChangePercentage: FC<ChangePercentageProps> = ({
     if (percentageChange < 0) {
         return (
             <ChangeContainer>
-                <CoinPrice>{endPrice}</CoinPrice>
+                <CoinPrice>${endPrice}</CoinPrice>
                 <ChangeValue change="down">
                     -{percentageChange.toFixed(2)}%
                 </ChangeValue>
@@ -43,7 +56,7 @@ const ChangePercentage: FC<ChangePercentageProps> = ({
     if (percentageChange > 0) {
         return (
             <ChangeContainer>
-                <CoinPrice>{endPrice}</CoinPrice>
+                <CoinPrice>${endPrice}</CoinPrice>
                 <ChangeValue change="up">
                     +{percentageChange.toFixed(2)}%
                 </ChangeValue>
@@ -53,7 +66,7 @@ const ChangePercentage: FC<ChangePercentageProps> = ({
     }
     return (
         <ChangeContainer>
-            <CoinPrice>{endPrice}</CoinPrice>
+            <CoinPrice>${endPrice}</CoinPrice>
             <ChangeValue>{percentageChange.toFixed(2)}%</ChangeValue>
         </ChangeContainer>
     );
