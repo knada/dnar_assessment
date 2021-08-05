cbf9f197a0c56728f89a4905ff1f01ca48c6ca68
Daniel Addae <nanaayimda@gmail.com>
2021-08-05 23:54:44 +0000

    Added custom tooltip for chart and chart header with date range picker and percentage change

--- src/components/CoinPage/CoinDataVisualization/CoinDataVisualization.tsx
+++ src/components/CoinPage/CoinDataVisualization/CoinDataVisualization.tsx
@@ -5,6 +5,7 @@ import {
     LineChart,
     ResponsiveContainer,
     Tooltip,
+    TooltipProps,
     XAxis,
     YAxis,
 } from "recharts";
@@ -13,12 +14,51 @@ import { useGetRangeDataQuery } from "../../../application/services/coins";
 import LoadingIndicator from "../../LoadingIndicator/LoadingIndicator";
 import {
     ChartContainer,
+    ChartHeader,
     CoinDataVisualizationContainer,
 } from "./CoinDataVisualization.styles";
 import ChangePercentage from "./ChangePercentage/ChangePercentage";
+import DateRange from "./DateRange/DateRange";
+import {
+    NameType,
+    ValueType,
+} from "recharts/types/component/DefaultTooltipContent";
+import styled from "styled-components";
 
 type CoinDataVisualitionProps = {
     id: string;
+};
+
+const TooltipContainer = styled.div`
+    background-color: ${props => props.theme.colors.buttonBg};
+    padding: 1.5rem;
+    border-radius: 1rem;
+    text-align: center;
+`;
+
+const TooltipPrice = styled.p`
+    font-size: 1.5rem;
+    font-weight: 500;
+`;
+
+const TooltipDate = styled.p`
+    font-size: 0.825rem;
+`;
+
+const CustomTooltip = ({
+    active,
+    payload,
+    label,
+}: TooltipProps<ValueType, NameType>) => {
+    if (active && payload && label) {
+        return (
+            <TooltipContainer>
+                <TooltipPrice>{`$${payload[0].value}`}</TooltipPrice>
+                <TooltipDate>{label}</TooltipDate>
+            </TooltipContainer>
+        );
+    }
+    return <TooltipContainer></TooltipContainer>;
 };
 
 const CoinDataVisualition: FC<CoinDataVisualitionProps> = ({ id }) => {
@@ -55,10 +95,13 @@ const CoinDataVisualition: FC<CoinDataVisualitionProps> = ({ id }) => {
         );
         return (
             <CoinDataVisualizationContainer>
-                <ChangePercentage
-                    endPrice={chartData[chartData.length - 1].price}
-                    percentageChange={percentageChange}
-                />
+                <ChartHeader>
+                    <ChangePercentage
+                        endPrice={chartData[chartData.length - 1].price}
+                        percentageChange={percentageChange}
+                    />
+                    <DateRange />
+                </ChartHeader>
                 <ChartContainer>
                     <ResponsiveContainer width="100%" height={300}>
                         <LineChart data={chartData}>
@@ -83,7 +126,10 @@ const CoinDataVisualition: FC<CoinDataVisualitionProps> = ({ id }) => {
                                 vertical={false}
                                 strokeDasharray="2 8"
                             />
-                            <Tooltip payload={chartData} />
+                            <Tooltip
+                                payload={chartData}
+                                content={CustomTooltip}
+                            />
                         </LineChart>
                     </ResponsiveContainer>
                 </ChartContainer>
