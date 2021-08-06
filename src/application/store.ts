d7e1fb89b95970646bcb93a9a440456bf8383d6c
Daniel Addae <nanaayimda@gmail.com>
2021-08-06 20:17:56 +0000

    Added dateReducer to store

--- src/application/store.ts
+++ src/application/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { coinApi } from "./services/coins";
+import dateReducer from "./features/dateSlice";
 
 const store = configureStore({
     reducer: {
         [coinApi.reducerPath]: coinApi.reducer,
+        date: dateReducer,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(coinApi.middleware),
