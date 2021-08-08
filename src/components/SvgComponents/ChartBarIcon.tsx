c4cd2c8923e28b0f7bb966540e8a004f130b6839
Daniel Addae <nanaayimda@gmail.com>
2021-08-08 02:42:34 +0000

    Added icons for side bar

---
+++ src/components/SvgComponents/ChartBarIcon.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import styled, { FlattenSimpleInterpolation } from "styled-components";
+
+type IconProps = {
+    iconStyle?: FlattenSimpleInterpolation;
+};
+
+const StyledSvg = styled.svg<IconProps>`
+    ${props => props.iconStyle};
+`;
+
+const ChartBarIcon: FC<IconProps> = ({ iconStyle }) => (
+    <StyledSvg
+        viewBox="0 0 24 24"
+        strokeWidth={1.25}
+        stroke="#2c3e50"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        iconStyle={iconStyle}
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <rect x={3} y={12} width={6} height={8} rx={1} />
+        <rect x={9} y={8} width={6} height={12} rx={1} />
+        <rect x={15} y={4} width={6} height={16} rx={1} />
+        <line x1={4} y1={20} x2={18} y2={20} />
+    </StyledSvg>
+);
+
+export default ChartBarIcon;
---
+++ src/components/SvgComponents/DashboardIcon.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import styled, { FlattenSimpleInterpolation } from "styled-components";
+
+type IconProps = {
+    iconStyle?: FlattenSimpleInterpolation;
+};
+
+const StyledSvg = styled.svg<IconProps>`
+    ${props => props.iconStyle};
+`;
+
+const DashboardIcon: FC<IconProps> = ({ iconStyle }) => (
+    <StyledSvg
+        viewBox="0 0 24 24"
+        strokeWidth={1.25}
+        stroke="#2c3e50"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        iconStyle={iconStyle}
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <circle cx={12} cy={13} r={2} />
+        <line x1={13.45} y1={11.55} x2={15.5} y2={9.5} />
+        <path d="M6.4 20a9 9 0 1 1 11.2 0z" />
+    </StyledSvg>
+);
+
+export default DashboardIcon;
