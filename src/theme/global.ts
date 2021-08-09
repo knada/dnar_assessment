d19da9993401b9b1c2fb406b833cc7f8650330e4
Daniel Addae <nanaayimda@gmail.com>
2021-08-09 02:07:30 +0000

    set background color on body

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -14,5 +14,6 @@ export const GlobalStyles = createGlobalStyle`
         color: white;
         text-rendering: optimizeLegibility;
         position: relative;
+        background-color: #201e26;
     }
 `;
