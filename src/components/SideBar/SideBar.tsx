25c6ec3f5dc7ed437bc481e8dbcc0d62576f0873
Daniel Addae <nanaayimda@gmail.com>
2021-08-09 02:16:03 +0000

    Added sidebar content

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,8 +1,54 @@
 import { FC } from "react";
-import { SideBarContainer } from "./SideBar.styles";
+import { css } from "styled-components";
+import ChartBarIcon from "../SvgComponents/ChartBarIcon";
+import DashboardIcon from "../SvgComponents/DashboardIcon";
+import MenuIcon from "../SvgComponents/MenuIcon";
+import {
+    SideBarBrand,
+    SideBarContainer,
+    SidebarHeader,
+    SideBarMenu,
+    SideBarMenuButton,
+    SideBarMenuIcon,
+    SideBarMenuItem,
+    SideBarMenuLink,
+    SideBarMenuText,
+} from "./SideBar.styles";
+
+const iconStyle = css`
+    height: 100%;
+    padding: 0;
+`;
 
 const SideBar: FC = () => {
-    return <SideBarContainer />;
+    return (
+        <SideBarContainer>
+            <SideBarMenu>
+                <SidebarHeader>
+                    <SideBarMenuButton>
+                        <MenuIcon iconStyle={iconStyle} />
+                    </SideBarMenuButton>
+                    <SideBarBrand>Logo</SideBarBrand>
+                </SidebarHeader>
+                <SideBarMenuItem>
+                    <SideBarMenuLink to="/">
+                        <SideBarMenuIcon>
+                            <DashboardIcon iconStyle={iconStyle} />
+                        </SideBarMenuIcon>
+                        <SideBarMenuText>Dashboard</SideBarMenuText>
+                    </SideBarMenuLink>
+                </SideBarMenuItem>
+                <SideBarMenuItem>
+                    <SideBarMenuLink to="/coins">
+                        <SideBarMenuIcon>
+                            <ChartBarIcon iconStyle={iconStyle} />
+                        </SideBarMenuIcon>
+                        <SideBarMenuText>Coin Details</SideBarMenuText>
+                    </SideBarMenuLink>
+                </SideBarMenuItem>
+            </SideBarMenu>
+        </SideBarContainer>
+    );
 };
 
 export default SideBar;
